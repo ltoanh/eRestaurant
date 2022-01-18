8c00d8d5b5c092d94bd15f084bb96cb3a9d1f695
whiwf <leetusoanh@gmail.com>
2022-01-18 21:52:50 +0700

    Add Header UI

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import Footer from 'components/footer/Footer';
 import Header from 'components/header/Header';
 import { BrowserRouter } from 'react-router-dom';
 import AppRoutes from 'routes/AppRoutes';
-import './App.css';
 
 function App() {
   return (
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,15 +1,46 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-import PATHS from 'routes/path'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import PATHS from 'routes/path';
+import './header.css';
+import logo from 'assets/images/logo.svg';
 
 function Header() {
-  return (
-    <div>
-      header
-      <NavLink to={PATHS.HOME_PAGE}>homepage</NavLink>
-      <NavLink to={PATHS.ABOUT_US}>about us</NavLink>
-    </div>
-  )
+	const user = {};
+
+	return (
+		<header id="header" className="container">
+			<div className="header__left">
+				<div className="header__logo">
+					<img src={logo} alt="Pizzdee logo" height="40px" />
+				</div>
+			</div>
+			<div className="header__middle">
+				<ul className="header__list">
+					<li className="header__list__item">
+						<NavLink className="header__link" to={PATHS.MENU}>Thực đơn</NavLink>
+					</li>
+					<li className="header__list__item">
+						<NavLink className="header__link" to={PATHS.SHOP}>Cửa hàng</NavLink>
+					</li>
+					<li className="header__list__item">
+						<NavLink className="header__link" to={PATHS.ABOUT_US}>Về chúng tôi</NavLink>
+					</li>
+				</ul>
+			</div>
+			<div className="header__right">
+				<NavLink className="header__link had-notification" to={PATHS.CART}>
+					<i className="ri-shopping-cart-2-line" />
+				</NavLink>
+				{user ? (
+					<NavLink className="header__link" to="/login">Đăng nhập</NavLink>
+				) : (
+					<NavLink className="header__link" to="/user/12334">
+						<i className="ri-user-3-line" />
+					</NavLink>
+				)}
+			</div>
+		</header>
+	);
 }
 
-export default Header
+export default Header;

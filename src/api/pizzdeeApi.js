4c996e5fde82ea5487d96595218f54cd0eb4197c
whiwf <leetusoanh@gmail.com>
2022-01-30 00:57:45 +0700

    Add homepage UI

--- src/api/pizzdeeApi.js
+++ src/api/pizzdeeApi.js
@@ -1,14 +1,18 @@
-import axiosClient from "./axiosClient";
+import axiosClient from './axiosClient';
 
 const URL_PRODUCTS = '/products';
 const URL_CATEGORIES = '/categories';
+const URL_RATINGS = '/ratings';
 
 const pizzdeeApi = {
-  getLatestProducts : (params) => {
-    return axiosClient.get(URL_PRODUCTS, params);
-  },
-  getCategories: (params) => {
-    return axiosClient.get(URL_CATEGORIES, params);
+	getLatestProducts: (params) => {
+		return axiosClient.get(URL_PRODUCTS, params);
+	},
+	getCategories: (params) => {
+		return axiosClient.get(URL_CATEGORIES, params);
+	},
+  getRatings: (params) => {
+    return axiosClient.get(URL_RATINGS, params);
   }
 };
 

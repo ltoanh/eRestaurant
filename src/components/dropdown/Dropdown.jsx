5a29db7d4895ab1ac1149ec358d9387472544e5c
whiwf <leetusoanh@gmail.com>
2022-02-13 14:42:54 +0700

    Add user dropdown UI

---
+++ src/components/dropdown/Dropdown.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import styled from 'styled-components';
+
+function Dropdown(props, ref) {
+  const {children} = props;
+  return (
+    <Wrapper ref={ref} className='dropdown'>
+      {children}
+    </Wrapper>
+  )
+}
+
+export default React.forwardRef(Dropdown);
+
+const Wrapper = styled.div`
+  position: absolute;
+  width: 200px;
+  left: 0;
+  transform: translateX(-50%);
+  margin-top: .5rem;
+  
+  border: 1px solid var(--gray);
+  border-radius: var(--border);
+
+  box-shadow: 3px 4px 8px -7px rgba(0, 0, 0, .7);
+
+  z-index: 10;
+`;
